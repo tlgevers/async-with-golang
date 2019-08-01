124f9ec0c396aaabb79a41180639e86dbf6b8aa3
Trevor Gevers <trevor.gevers@ditoweb.com>
2019-08-01 08:44:03 -0500

    update function name, test async function/var assignment

--- main.go
+++ main.go
@@ -10,11 +10,25 @@ package helloworld
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-// GoStarter is an HTTP Cloud Function.
-func GoStarter(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!")
+func task1(task string) {
+	time.Sleep(1 * time.Second)
+	task = "task1"
+}
+
+func task2(task string) {
+	time.Sleep(2 * time.Second)
+	task = "task2"
+}
+
+// ProcessQuery is an HTTP Cloud Function.
+func ProcessQuery(w http.ResponseWriter, r *http.Request) {
+	var task string
+	go task1(task)
+	go task2(task)
+	fmt.Fprint(w, task)
 }
 
 // [END functions_helloworld_get]

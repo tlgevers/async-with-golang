07a2cdc651cf2f1b1d0c7f64ba21d35070f62004
Trevor Gevers <trevor.gevers@ditoweb.com>
2020-01-17 09:45:26 -0600

    refactor, move away from cloud function

--- main.go
+++ main.go
@@ -1,37 +1,26 @@
-// Copyright 2018 Google LLC. All rights reserved.
-// Use of this source code is governed by the Apache 2.0
-// license that can be found in the LICENSE file.
-
-// [START functions_helloworld_get]
-
-// Package helloworld provides a set of Cloud Function samples.
-package helloworld
+package main
 
 import (
-	"fmt"
-	"net/http"
-	"time"
+"fmt"
+"time"
 )
 
-// var PROJECT_ID = "develop-testing-bed"
+func sum(a int, b int, c chan int) {
 
-func task1(ch chan string) {
-	time.Sleep(0 * time.Second)
-	ch <- "task1"
+	c <- a + b // send sum to c
 }
 
-func task2(ch chan string) {
-	time.Sleep(0 * time.Second)
-	ch <- "task2"
+func sum1(a int, b int, c chan int) {
+        time.Sleep(100 * time.Millisecond)
+
+	c <- a + b // send sum to c
 }
 
-// ProcessQuery is an HTTP Cloud Function.
-func ProcessQuery(w http.ResponseWriter, r *http.Request) {
-	//ch := make(chan string)
-	//go task1(ch)
-	//go task2(ch)
-	//task := <-ch
-	fmt.Fprint(w, "TESTING RESPONSE TIME FOR BASIC TEXT")
-}
+func main() {
+	c := make(chan int)
+	go sum1(10, 10, c)
+	go sum(20, 22, c)
+	x := <-c // receive from c
 
-// [END functions_helloworld_get]
+	fmt.Println(x)
+}

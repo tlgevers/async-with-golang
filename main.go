721e8e3d6e979e63a38ce842041d7d7b5384d75b
Trevor Gevers <trevor.gevers@ditoweb.com>
2020-01-17 10:48:27 -0600

    capture both values of the go routines from the channel

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-"fmt"
-"time"
+    "fmt"
+    "time"
 )
 
 func sum(a int, b int, c chan int) {
 
-	c <- a + b // send sum to c
+    c <- a + b // send sum to c
 }
 
 func sum1(a int, b int, c chan int) {
-        time.Sleep(100 * time.Millisecond)
+    time.Sleep(100 * time.Millisecond)
 
-	c <- a + b // send sum to c
+    c <- a + b // send sum to c
 }
 
 func main() {
-	c := make(chan int)
-	go sum1(10, 10, c)
-	go sum(20, 22, c)
-	x := <-c // receive from c
+    c := make(chan int)
+    go sum1(10, 10, c)
+    go sum(20, 22, c)
+    x, y := <-c, <-c // receive from c
 
-	fmt.Println(x)
+    fmt.Println(x, y)
 }
